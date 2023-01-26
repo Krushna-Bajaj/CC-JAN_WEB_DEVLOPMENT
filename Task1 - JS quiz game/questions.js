70b364ac6cb88d0d943ff570fea14a2b80b841cd
Krushna-Bajaj <sujaybajaj26@gmail.com>
2023-01-26 17:21:51 +0530

    Code Clause Project

---
+++ Task1 - JS quiz game/questions.js
@@ -0,0 +1,125 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which one of the following is the correct way for calling the JavaScript code?",
+    answer: "Function/Method",
+    options: [
+      "Preprocessor",
+      "Triggering Event",
+      "RMI",
+      "Function/Method"
+    ]
+  },
+    {
+    numb: 2,
+    question: " Which type of JavaScript language is ___",
+    answer: "Object-Based",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Assembly-language",
+      "High-level"
+    ]
+  },
+    {
+    numb: 3,
+    question: " Which one of the following also known as Conditional Expression:",
+    answer: "immediate if",
+    options: [
+      "Alternative to if-else",
+      "Switch statement",
+      "If-then-else statement",
+      "immediate if"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In JavaScript, what is a block of statement?",
+    answer: "block that combines a number of statements into a single compound statement",
+    options: [
+      "Conditional block",
+      "block that combines a number of statements into a single compound statement",
+      "both conditional block and a single statement",
+      "block that contains a single statement"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The function and var are known as:",
+    answer: "Declaration statements",
+    options: [
+      "Keywords",
+      "Data types",
+      "Declaration statements",
+      "Prototypes"
+    ]
+  },
+    {
+    numb: 6,
+    question: "When interpreter encounters an empty statements, what it will do:",
+    answer: "Ignores the statements",
+    options: [
+      "Shows a warning",
+      "Prompts to complete the statement",
+      "Throws an error",
+      "Ignores the statements"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which of the following type of a variable is volatile?",
+    answer: "Mutable variable",
+    options: [
+      "Mutable variable",
+      "Dynamic variable",
+      "Volatile variable",
+      "Immutable variable"
+    ]
+  },
+    {
+    numb: 8,
+    question: "When there is an indefinite or an infinite value during an arithmetic computation in a program, then JavaScript prints______.",
+    answer: "Displays Infinity",
+    options: [
+      "Prints an exception error",
+      "Prints an overflow error",
+      "Displays Infinity",
+      "Prints the value as such"
+    ]
+  },
+    {
+    numb: 9,
+    question: "In the JavaScript, which one of the following is not considered as an error:",
+    answer: "Division by zero",
+    options: [
+      "Syntax error",
+      "Missing of semicolons",
+      "Division by zero",
+      "Missing of Bracket"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Which of the following givenfunctions of the Number Object formats a number with a different number of digits to the right of the decimal?",
+    answer: "toFixed()",
+    options: [
+      "toExponential()",
+      "toFixed()",
+      "toPrecision()",
+      "toLocaleString()"
+    ]
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
